00dc4de74b93fecf2df138cadb0b23b2cffef28f
lilran14 <radans.works@gmail.com>
2021-06-23 20:32:54 +0700

    Add Theme Config

---
+++ lib/core/theme/theme_config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+class ThemeColor {
+  // General
+  // == Accent Color ==
+  static final Color accentColor = Color(0xFFFE5E5E);
+  // Dark Theme
+  static final Color darkPrimaryColor = Color(0xFF232232);
+  static final Color darkPrimaryVariantColor = Color(0xFFBABABA);
+  static final Color darkBackgroundColor = Color(0xFF201A2D);
+  static final Color darkBackgroundVariantColor = Color(0xFF272A3C);
+  static final Color darkTextColor = Color(0xFFFFFFFF);
+  static final Color darkTextVariantColor = Color(0xFFBABABA);
+
+  // Semantic Color
+  static final Color semanticGreenColor = Color(0xFF9BFE5E);
+  static final Color semanticOrangeColor = Color(0xFFFE7B5E);
+  static final Color semanticRedColor = Color(0xFFFE5E5E);
+  static final Color semanticYellowColor = Color(0xFFF1FE5E);
+  static final Color semanticBlueColor = Color(0xFF5E78FE);
+  static final Color semanticPurpleColor = Color(0xFFAE5EFE);
+}
+
+class Gap {
+  static final double defaultPadding = 20;
+  static final double defaultMargin = 20;
+  static final double defaultGap = 10;
+}
