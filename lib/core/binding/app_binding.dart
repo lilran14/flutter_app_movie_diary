2b679863af2fe8d7dd75828d29c14fa8f3b7acf5
lilran14 <radans.works@gmail.com>
2021-06-28 22:40:26 +0700

    Update App Binding

--- lib/core/binding/app_binding.dart
+++ lib/core/binding/app_binding.dart
@@ -1,11 +1,32 @@
 import 'package:flutter_app_movie_diary/controllers/auth_controller.dart';
 import 'package:flutter_app_movie_diary/controllers/home_controller.dart';
+import 'package:flutter_app_movie_diary/controllers/list_movie_controller.dart';
 import 'package:get/get.dart';
 
 class MainPageBinding implements Bindings {
   @override
   void dependencies() {
     Get.put<HomeController>(HomeController());
+    Get.put<ListMovieController>(
+      ListMovieController(listType: ListType.popular),
+      tag: "Popular",
+      permanent: true,
+    );
+    Get.put<ListMovieController>(
+      ListMovieController(listType: ListType.nowplaying),
+      tag: "NowPlaying",
+      permanent: true,
+    );
+    Get.put<ListMovieController>(
+      ListMovieController(listType: ListType.toprated),
+      tag: "TopRated",
+      permanent: true,
+    );
+    Get.put<ListMovieController>(
+      ListMovieController(listType: ListType.upcoming),
+      tag: "UpComing",
+      permanent: true,
+    );
   }
 }
 
